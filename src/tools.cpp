8e123f82924fa907b349598e3c6efe01e833f881
Akash Chauhan <a-chauhan@mail.nissan.co.jp>
2019-04-07 04:00:41 +0900

    WIP

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include "tools.h"
 
 using Eigen::VectorXd;
 using std::vector;
+using namespace std;
 
 Tools::Tools() {}
 
@@ -12,4 +14,24 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  
+  if(estimations.size() == 0){
+    return rmse;
+  }
+
+  if(estimations.size() != ground_truth.size()){
+    return rmse;
+  }
+
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+  
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
